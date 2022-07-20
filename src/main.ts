d396afecca2eeba97f0518c2ea8060225dafc439
nauxscript mac-mini <nauxscript@gmail.com>
2022-07-20 22:36:38 +0800

    :construction: testing how to check the custom component use in page but not register

--- src/main.ts
+++ src/main.ts
@@ -3,15 +3,14 @@ import "./style/prism.css";
 import "uno.css";
 import { html as test } from './md/test.md'
 import { factory } from "./utils/markdownHelper";
-import { getCustomElements, hijackDefine, getRegisteredElements } from './utils/index';
+import { hijackDefine, notRegisterElementWarning } from './utils/index';
 
 hijackDefine()
+
+notRegisterElementWarning()
 
 factory({
   name: 'first-md-com',
   innerHtml: test,
   containerClassName: 'test'
 }).use()
-
-console.log(getCustomElements())
-console.log(getRegisteredElements())
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -6,19 +6,37 @@ export const getCustomElements = () =>
     .map((element) => element.nodeName.toLowerCase())
     .filter(customElements.get.bind(customElements));
 
-
 export const getRegisteredElements = () => {
-  return window.customTags
-}
-    
+  return window.customTags;
+};
+
+export const notRegisterElementWarning = () => {
+  const targetNode = document.getElementById("app");
+  if (!targetNode) return
+  // 观察器的配置（需要观察什么变动）
+  const config = { childList: true, subtree: true };
+  // 创建一个观察器实例并监听`targetNode`元素的变动
+  const observer = new MutationObserver(() => {
+    console.log('app dom tree change!')
+    console.log(getCustomElements())
+    console.log(window.customTags)
+    // const tags = window.customTags.map(item => item.tag)
+    // const notUsedComs = getCustomElements().filter(item => {
+    //   return !tags.includes(item)
+    // })
+    // notUsedComs.map(item => {
+    //   console.error(`Component ${item} is not register!`)
+    // })
+  });
+  observer.observe(targetNode, config)
+  return observer
+};
 
 export const hijackDefine = () => {
   const original = window.customElements.define;
-  window.customTags = [] 
+  window.customTags = [];
   window.customElements.define = function (...args: TDefineArgs) {
     window.customTags.push({ tag: args[0], ctor: args[1] });
     original.apply(customElements, args);
   };
-
-}
-
+};
